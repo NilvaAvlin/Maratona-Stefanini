8458f0717ccd4de7af73b2a048a49fe8a98a5f7c
NilvaRodrigues <avlin.nilva@gmail.com>
2020-10-09 10:55:02 -0300

    Código até a pausa

--- projeto-semana-angularjs-dia-3-base-explicacao/lista-compras.js
+++ projeto-semana-angularjs-dia-3-base-explicacao/lista-compras.js
@@ -4,6 +4,11 @@
     // MODULO
     angular.module('listaTarefasApp', []);
 
+    angular.module('listaTarefasApp')
+    .filter('capitalize', function (){
+        return function(input) {return input.charAt(0).toUpperCase() + input.substring(1);}
+    })
+
 
     // CONTROLLER
     angular.module('listaTarefasApp')
@@ -11,21 +16,27 @@
 
     listaTarefasController.$inject = ['$rootScope'];
 
-    function listaTarefasController($rootScope) {
+    function listaTarefasController(root) {
         var vm = this;
         /* ***************    INIT VARIÁVEIS    *********************************** */
         vm.logado = false;
         vm.validar = false;
         vm.register = false;
 
-        $rootScope.listaMensagens = [
-            { text: 'Mensagem Aqui', tipo: 'danger' },
-            { text: 'Mensagem Aqui', tipo: 'info' },
+        root.listaMensagens = [{ text: 'TESTE MENSAGEM ..teste', tipo: 'success' },];
+        vm.teste = 'teste';
+    
+        /*    { text: 'Mensagem Aqui', tipo: 'info' },
             { text: 'Mensagem Aqui', tipo: 'success' },
             { text: 'Mensagem Aqui', tipo: 'warning' },
-        ]; 
+        ]; */
 
         /* ***************    FUNÇÕES EXECUTADAS NA VIEW (HMTL)    **************** */
+        vm.alerta = alerta;
+
+        function alerta(){
+            vm.login.username = vm.login.username.toUpperCase();
+        }
 
 
         /* ***************    FUNÇÕES INSTERNAS    ******************************** */
